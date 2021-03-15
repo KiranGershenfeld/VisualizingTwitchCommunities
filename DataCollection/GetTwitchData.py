6d64e6547f434e9f0e14a867238b6f8531887cf7
Anton Bulakh <self@necauqua.dev>
2021-03-15 13:54:16 +0200

    Further normalize that api by moving credentials to the application level

--- DataCollection/GetTwitchData.py
+++ DataCollection/GetTwitchData.py
@@ -1,17 +1,12 @@
-import json
-
 import requests
 
 
 # Gets the count top streams currently live on twitch. numberOfStreams max is 100
-def get_top_streamers(count):
+def get_top_streamers(credentials, count):
     print("Getting a list of top live streams...")
 
-    # temporary so it at least just works for me
-    cr = json.load(open('../credentials.json'))
-
     # Header auth values taken from twitchtokengenerator.com, not sure what to do if they break
-    headers = {'Client-ID': cr['client-id'], 'Authorization': f"Bearer {cr['access-token']}"}
+    headers = {'Client-ID': credentials['client-id'], 'Authorization': f"Bearer {credentials['access-token']}"}
 
     # Request top `count` viewed streams on twitch
     data = requests.get(f'https://api.twitch.tv/helix/streams?first={count}', headers=headers).json()
@@ -27,7 +22,6 @@ def get_current_viewers(channel):
     try:
         r = requests.get(f'http://tmi.twitch.tv/group/user/{channel.lower()}/chatters').json()
     except:
-        # If the query couldnt be completed return None
         return None
 
     # List consists of users in chat tagged as viewer or VIP
--- DataCollection/main.py
+++ DataCollection/main.py
@@ -1,12 +1,18 @@
 import CSVWriting
 import GetTwitchData
+import json
 
 
 def main():
+    # our credentials
+    cr = json.load(open('../credentials.json'))
+
     # Get the top 100 streamers on Twitch
-    streamers = GetTwitchData.get_top_streamers(100)
+    streamers = GetTwitchData.get_top_streamers(cr, count=100)
+
     # Create a dictionary of {streamer:[viewers]} from those 100 streams
     data = GetTwitchData.get_viewer_map(streamers)
+
     # Add that dictionary to the master csv
     CSVWriting.update_twitch_data(data)
 
