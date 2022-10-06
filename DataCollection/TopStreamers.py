9e00b83766ab70e6d492dfc6147509aed832048b
Kiran Desktop <kiran@gershenfeld.org>
2022-10-06 12:19:46 -0700

    Restructuring for automation overhaul

---
+++ DataCollection/TopStreamers.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def lambda_handler(event, context):
+    pass
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
